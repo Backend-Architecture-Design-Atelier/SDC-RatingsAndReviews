3434ec6c2cf484618b507a058972569edfa4b140
Neil <neiljohnson92@gmail.com>
2022-06-12 16:16:35 -0700

    Finished add review query

--- server/models/reviews.js
+++ server/models/reviews.js
@@ -11,9 +11,10 @@ module.exports = {
     if (sort === 'newest') {
       sort = 'date';
     }
-
+    const offset = (page * count) - count;
+    const queryArgs = [product_id, sort, count, offset];
     const queryString = `
-    SELECT reviews.id, rating, summary, recommend, response, body, date(to_timestamp(date / 1000)::date), reviewer_name, helpfulness,
+    SELECT reviews.id, rating, summary, recommend, response, body, to_char(to_timestamp(date / 1000), 'yyyy-MM-dd"T"00:00:00.000Z') as date, reviewer_name, helpfulness,
 
     COALESCE (
       ARRAY_AGG (
@@ -27,16 +28,14 @@ module.exports = {
     FROM reviews
     LEFT JOIN photos ON reviews.id = photos.review_id
 
-    WHERE product_id = ${product_id}
+    WHERE product_id = $1
     GROUP BY reviews.id
-    ORDER BY ${sort} DESC
-    LIMIT ${count}
-    OFFSET ${(page * count) - count}
+    ORDER BY $2 DESC
+    LIMIT $3
+    OFFSET $4
     `;
 
-    //Fix date formatting
-
-    pool.query(queryString, (err, results) => {
+    pool.query(queryString, queryArgs, (err, results) => {
       if (err) {
         console.log(err.stack)
         callback(err.stack);
@@ -58,6 +57,7 @@ module.exports = {
     const queryString =`
       INSERT INTO reviews (product_id, rating, summary, body, recommend, reviewer_name, reviewer_email)
       VALUES ($1, $2, $3, $4, $5, $6, $7)
+      RETURNING id
       `;
 
       pool.query(queryString, queryArgs, (err, reviewId) => {
@@ -66,51 +66,61 @@ module.exports = {
           callback(err.stack);
         }
 
-        console.log('REVIEW ID', reviewId)
+        reviewId = reviewId.rows[0].id;
 
-        // if (photos.length > 0) {
-        //   photos.forEach((photo) => {
-        //     const photosQueryString =`
-        //       INSERT INTO photos (review_id, url)
-        //       VALUES (${reviewId}, ${photo})
-        //     `;
+        if (photos.length > 0) {
+          photos.forEach((photo) => {
+            const photoQueryArgs = [reviewId, photo]
+            const photosQueryString =`
+              INSERT INTO photos (review_id, url)
+              VALUES ($1, $2)
+            `;
 
-        //     pool.query(photosQueryString, (err) => {
-        //       if (err) {
-        //         callback(err.stack);
-        //       }
-        //     });
-        //   })
-        // }
+            pool.query(photosQueryString, photoQueryArgs, (err, results) => {
+              if (err) {
+                console.log(err.stack);
+                callback(err.stack);
+              }
+            });
+          })
+        }
 
-        // const characteristicsQueryString =`
-        //   SELECT id FROM characteristics WHERE product_id = ${product_id}
-        // `;
+        const characteristicsQueryString =`
+          SELECT id FROM characteristics WHERE product_id = ${product_id}
+        `;
 
-        // pool.query(characteristicsQueryString, (err, characteristicIds) => {
-        //   if (err) {
-        //     callback(err.stack);
-        //   }
-        //   console.log('CHARACTERISTIC IDS',characteristicIds)
-        //   const characteristicKeys = Object.keys(characteristics);
+        pool.query(characteristicsQueryString, (err, characteristicIds) => {
+          if (err) {
+            console.log(err.stack);
+            callback(err.stack);
+          }
 
-        //   if (characteristicIds.length > 0) {
-        //     characteristicIds.forEach((id, index) => {
-        //       const characteristicReviewsQuery =`
-        //         INSERT INTO characteristic_reviews (characteristic_id, review_id, value)
-        //         VALUES (${id}, ${reviewId}, ${characteristics[characteristicKeys[index]]})
-        //       `;
+          characteristicIds = characteristicIds.rows
+          const characteristicKeys = Object.keys(characteristics);
 
-        //       pool.query(characteristicReviewsQuery, (err, newReview) => {
-        //         if (err) {
-        //           callback(err.stack);
-        //         } else {
-        //           callback(null, newReview);
-        //         }
-        //       });
-        //     })
-        //   }
-        // });
+          if (characteristicIds.length > 0 && characteristicKeys.length > 0) {
+            characteristicIds.forEach((characteristic, index) => {
+              const characteristic_id = characteristic.id;
+
+              if (characteristicKeys.indexOf(characteristic_id + '') > -1) {
+                const characteristicReviewsQueryArgs = [characteristic_id, reviewId, characteristics[characteristic_id]]
+                const characteristicReviewsQuery =`
+                  INSERT INTO characteristic_reviews (characteristic_id, review_id, value)
+                  VALUES ($1, $2, $3)
+                  RETURNING *
+                `;
+
+                pool.query(characteristicReviewsQuery, characteristicReviewsQueryArgs, (err, newReview) => {
+                  if (err) {
+                    console.log(err.stack);
+                    callback(err.stack);
+                  }
+                });
+              }
+            });
+          }
+        });
+        callback(null, reviewId);
       });
     }
   };
