647253aa6817e45e2560c80eec8fdd7015148d65
Neil <neiljohnson92@gmail.com>
2022-06-13 16:30:37 -0700

    Added indexes to schema

--- database/schema.sql
+++ database/schema.sql
@@ -41,6 +41,33 @@ CREATE TABLE characteristic_reviews (
   value SMALLINT NOT NULL
 );
 
+DROP INDEX IF EXISTS product_id_index;
+CREATE INDEX product_id_index on reviews (product_id);
+
+DROP INDEX IF EXISTS review_id_index;
+CREATE INDEX review_id_index on reviews (id);
+
+DROP INDEX IF EXISTS photo_id_index;
+CREATE INDEX photo_id_index on photos (id);
+
+DROP INDEX IF EXISTS p_review_id_index;
+CREATE INDEX p_review_id_index on photos (review_id);
+
+DROP INDEX IF EXISTS photo_id_index;
+CREATE INDEX photo_id_index on photos (id);
+
+DROP INDEX IF EXISTS p_review_id_index;
+CREATE INDEX p_review_id_index on photos (review_id);
+
+DROP INDEX IF EXISTS characteristics_id_index;
+CREATE INDEX characteristics_id_index on characteristics (id);
+
+DROP INDEX IF EXISTS characteristics_product_id_index;
+CREATE INDEX CONCURRENTLY characteristics_product_id_index on characteristics (product_id);
+
+DROP INDEX IF EXISTS c_reviews_characteristic_id_index;
+CREATE INDEX c_reviews_characteristic_id_index on characteristic_reviews (characteristic_id);
+
 COPY reviews FROM '/Users/neil/hr-immersive/sdc/SDC-RatingsAndReviews/example_data/reviews.csv' DELIMITER ',' CSV Header;
 COPY photos FROM '/Users/neil/hr-immersive/sdc/SDC-RatingsAndReviews/example_data/reviews_photos.csv' DELIMITER ',' CSV Header;
 COPY characteristics FROM '/Users/neil/hr-immersive/sdc/SDC-RatingsAndReviews/example_data/characteristics.csv' DELIMITER ',' CSV Header;
